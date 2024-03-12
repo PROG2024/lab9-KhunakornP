41ec42ddf4194732cf3da24f235dec8f83848258
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-03-12 15:45:12 +0700

    Added test cases for class Circle

--- test_circle.py
+++ test_circle.py
@@ -8,5 +8,29 @@ Write unit tests as described in README.md.
 
 """
 from circle import Circle
+import unittest
+from math import hypot
 
-# TODO write 3 tests as described above
+
+class CircleTest(unittest.TestCase):
+    """Tests for the circle class"""
+    def setUp(self):
+        self.circle = Circle(5)
+
+    def test_add_positive_area_to_circle(self):
+        """Add two circles together"""
+        new_circle = self.circle.add_area(Circle(10))
+        self.assertEqual(Circle(hypot(10, 5)).get_area(), new_circle.get_area())
+        new_circle = self.circle.add_area(Circle(42))
+        self.assertEqual(hypot(42, 5), new_circle.get_radius())
+
+    def test_add_zero_to_circle(self):
+        """Add a circle with radius zero to the circle"""
+        new_circle = self.circle.add_area(Circle(0))
+        self.assertEqual(self.circle.get_radius(), new_circle.get_radius())
+        self.assertEqual(self.circle.get_area(), new_circle.get_area())
+
+    def test_create_negative_radius_circle(self):
+        """Circle should raise ValueError if radius is less than zero"""
+        with self.assertRaises(ValueError):
+            new_circle = Circle(-1)
