c65a1525219ac345e03d3e3844c3e9516fcc1c29
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-03-13 12:19:33 +0700

    Added test cases for counter

--- counter/test_counter.py
+++ counter/test_counter.py
@@ -7,3 +7,25 @@
 
    You can use pytest or unittest.
 """
+import unittest
+from counter import Counter
+
+
+class CounterTest(unittest.TestCase):
+    def test_counter_is_singleton(self):
+        """Counter should have the same id"""
+        original_id = id(Counter())
+        counter2 = Counter()
+        self.assertEqual(original_id, id(counter2))
+        self.assertTrue(Counter() == counter2)
+
+    def test_counter_remembers_count(self):
+        """Counter should be able to increment and keep its count"""
+        counter = Counter()
+        self.assertEqual(0, counter.count)
+        counter.increment()
+        self.assertEqual(1, counter.count)
+        counter.increment()
+        self.assertEqual(2, counter.count)
+        counter2 = Counter()
+        self.assertEqual(2, counter2.count)
